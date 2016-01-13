8286e2e47b7f7efecfe34034e49ea42603bf5c42
queni <queni@85.27.21.189>
2016-01-13 13:02:56 +0100

    poster column added to db and to resultListFiller

--- resultListFiller.php
+++ resultListFiller.php
@@ -9,10 +9,11 @@
     function resultListFiller($tableData) 
     {
         $row = count($tableData);
-        $indexToUse = Array(1,2,3);
+        $indexToUse = Array(1,2,3,4);
 //        $indexToUse = Array(0,1,2,3);
 //        $IMG = $$tableData[$i][$indexToUse[4]]; 
         $IMG = 'DbAffiches/Affiche1.jpg';
+        $path = 'DbAffiches/';
         echo '<table class="resultList">';
         for($i=0;$i<$row;++$i)
         {
@@ -34,7 +35,7 @@
 //            echo " (".$tableData[$i][1].") </h3>"; 
 //            echo $tableData[$i][2]; 
 //            
-            echo '<img src="'.$IMG.'" alt=""/>';
+            echo '<img src="'.$path.$tableData[$i][$indexToUse[3]].'" alt=""/>';
             echo "<h3>".$tableData[$i][$indexToUse[0]].""; 
             echo " (".$tableData[$i][$indexToUse[1]].") </h3>"; 
             echo $tableData[$i][$indexToUse[2]]; 
