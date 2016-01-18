0696923b9c7d3bd072b95251b0846f1acd820769
queni <queni@85.27.82.11>
2016-01-18 19:50:00 +0100

    SCSS folder added cssGen folder added css files converted into scss

--- CSSInclude.php
+++ CSSInclude.php
@@ -1,7 +1,15 @@
-
-<link href="css/cinema.css" rel="stylesheet" type="text/css"/>
+<?php   $path = "css/"; 
+        $path = "cssGen/";
+        ?>
+<!--<link href="css/cinema.css" rel="stylesheet" type="text/css"/>
 <link href="css/navigationBar.css" rel="stylesheet" type="text/css"/>
 <link href="css/resultList.css" rel="stylesheet" type="text/css"/>
 <link href="css/responsiveTable.css" rel="stylesheet" type="text/css"/>
 <link href="css/gridTable.css" rel="stylesheet" type="text/css"/>
-<link href="css/filmContent.css" rel="stylesheet" type="text/css"/>
+<link href="css/filmContent.css" rel="stylesheet" type="text/css"/>-->
+<link href="<?php echo $path; ?>cinema.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $path; ?>navigationBar.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $path; ?>resultList.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $path; ?>responsiveTable.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $path; ?>gridTable.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $path; ?>filmContent.css" rel="stylesheet" type="text/css"/>
